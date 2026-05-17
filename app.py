bde17a44502171232b7cc09fa43abf40536ffcaf
danche-dev <dane.devstuff@gmail.com>
2026-05-17 11:12:42 +0200

    Update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("🏃 Valcon Step Challenge 2026")
 df = pd.read_csv("data.csv", encoding="utf-8-sig")
 
 # 2. Individual Steps Chart (Grouped by Team Color)
-st.subheader("Stanje na dan 16.05.2026, 11:30 h")
+st.subheader("Stanje na dan 17.05.2026, 11:00 h")
 # 'color' ensures same team = same color
 # 'hover_data' adds the date or other info to the tooltip
 fig_ind = px.bar(
